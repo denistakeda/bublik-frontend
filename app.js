b4db1dbacc0db972adcfc34235e92e00141fd483
Denis Krivosheev <denis.takeda@gmail.com>
2014-07-09 22:03:54 +0600

    Bagel as loader

--- app.js
+++ app.js
@@ -9,17 +9,17 @@ var lessMiddleware = require('less-middleware');
 if ( ~process.argv.slice(2).indexOf('--production') )	{
 	// production
 	console.log('production')
-	app.use('/inforbix/web/',express.static( __dirname + "/target/build", { maxAge: 3600 }) );
+	app.use('/',express.static( __dirname + "/target/build", { maxAge: 3600 }) );
 } else {
 	// development
 	console.log('development');
 	app.use(lessMiddleware({
 		src: __dirname + '/src',
 		dest: __dirname + "/src",
-		prefix: '/inforbix/web',
+		prefix: '/',
 		compress: true
 	}));
-    app.use('/inforbix/web/',express.static(__dirname + "/src", { maxAge: false }));
+    app.use('/',express.static(__dirname + "/src", { maxAge: false }));
 }
 
 app.get(/\/steal.*/, function(req, res){
