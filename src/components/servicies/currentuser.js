51e63acd3e324cfb85a3f141d5f85de147a4ff1b
Denis Krivosheev <krivosheev.denis@autodesk.com>
2014-07-28 12:16:53 +0600

    feat(config): Added application config

--- src/components/servicies/currentuser.js
+++ src/components/servicies/currentuser.js
@@ -1,20 +1,21 @@
 define([
 	"bublikApp",
-	"angular"
+	"angular",
+	'components/servicies/config'
 ], function(app){
 	"use strict";
-	var service = function($interval, resources){
+	var service = function($interval, resources, config){
 		var user = {};
 		$interval(function(){
 			resources.currentUser.get({}, function(response){
 				angular.extend(user, response);
 			});
-		}, 5000)
+		}, config.userPingTimeout);
 
 		return user;
 	};
 
-	service.$inject = ["$interval", "resources"];
+	service.$inject = ["$interval", "resources", "glxConfig"];
 	app.factory("currentUser", service);
 
 	return service;
