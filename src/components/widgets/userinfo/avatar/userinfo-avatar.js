7028466fb51d090c9c11963bf4835f0a1a341995
Denis Krivosheev <denis.takeda@gmail.com>
2014-08-03 23:42:29 +0600

    feat(avatar): Send avatar to backend

--- src/components/widgets/userinfo/avatar/userinfo-avatar.js
+++ src/components/widgets/userinfo/avatar/userinfo-avatar.js
@@ -5,20 +5,32 @@ define([
 	'css!components/widgets/userinfo/avatar/userinfo-avatar.css'
 ], function(app){
 
-	var factory = function(){
+	var factory = function(backend){
 		return function($scope, $modalInstance){
+			var avatar = {};
 			$scope.ok = function(){
+				backend.updateUserAvatar(avatar);
+				avatar = {};
+				$modalInstance.close();
+			};
+
+			$scope.cancel = function(){
 				$modalInstance.close();
 			};
 
 			$scope.onLoadImage = function(imgData){
-			}
+				avatar.data = imgData;
+			};
 
-			$scope.onCropImage = function(x, y, w){
+			$scope.onCropImage = function(x, y, l){
+				avatar.x = x;
+				avatar.y = y;
+				avatar.l = l;
 			}
 		}
 	};
 
+	factory.$inject = ["backend"];
 	app.factory('userInfoAvatarCtrl', factory);
 
 });
