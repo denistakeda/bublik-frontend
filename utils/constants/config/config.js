21d458cff510d43532c87be151eb2f763d13e7a8
Vladislav Aituganov <vaituganov@gmail.com>
2014-08-28 18:10:28 +0600

    feat(sideMenu): add new menu items

--- utils/constants/config/config.js
+++ utils/constants/config/config.js
@@ -8,6 +8,8 @@ angular.module('glxUtils').constant('glxConfig',{
     defaultAvatar: "imgs/def_avatar.png",
     menuItems: {
         selfpage: function(id){ return {link: "/user/" + id, label: menuItemTitlePrefix + 'selfpage', classes: 'glyphicon glyphicon-user'}},
-        companies: function(id){ return {link: "/user/" + id + "/companies", label: menuItemTitlePrefix + 'companies', classes: 'glyphicon glyphicon-home'}}
+        companies: function(id){ return {link: "/user/" + id + "/companies", label: menuItemTitlePrefix + 'companies', classes: 'glyphicon glyphicon-home'}},
+        followers: function(id){ return {link: "/user/" + id + "/followers", label: menuItemTitlePrefix + 'followers', classes: 'glyphicon glyphicon-thumbs-up'}},
+        followed: function(id){ return {link: "/user/" + id + "/followed", label: menuItemTitlePrefix + 'followed', classes: 'glyphicon glyphicon-star'}}
     }
 });
