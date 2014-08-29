39205f94353c366aec44e0dce9f29c56c4618c42
Vladislav Aituganov <vaituganov@gmail.com>
2014-08-29 08:20:39 +0600

    feat(company): start work with company registration form

--- pages/companyRegistration/companyRegistration.js
+++ pages/companyRegistration/companyRegistration.js
@@ -1,10 +1,14 @@
-angular.module('glxPages').directive('glxCompanyRegistration', function () {
+angular.module('glxPages').directive('glxCompanyRegistration', function (glxConfig) {
     return {
         restrict: 'E',
         scope: {},
         templateUrl: 'pages/companyRegistration/companyRegistration.html',
         link: function (scope, elm, attrs, fn) {
-
+            scope.company = {
+                title: '',
+                slogan: '',
+                hidden: true
+            };
         }
     };
 });
