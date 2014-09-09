88515ed10898fe07ffd9d96fbdf724d482639aad
den <denis.takeda@gmail.com>
2014-09-09 22:42:26 +0600

    chore(karma): configuration for directive testing

--- templates/directive/complex/directive-spec.js
+++ templates/directive/complex/directive-spec.js
@@ -1,21 +1,20 @@
 describe('<%= "glx"+_.capitalize(name) %>', function () {
 
     beforeEach(module('<%= appname %>'));
+    beforeEach(module('html.templates'));
 
-    var scope, compile;
+    var scope, element;
 
     beforeEach(inject(function ($rootScope, $compile) {
         scope = $rootScope.$new();
-        compile = $compile;
+        element = angular.element('<<%= "glx-"+_.dasherize(name) %>></<%= "glx-"+_.dasherize(name) %>>');
+        $compile(element)(scope);
+        scope.$digest();
     }));
 
     it('should ...', function () {
 
         /*
-         To test your directive, you need to create some html that would use your directive,
-         send that through compile() then compare the results.
-
-         var element = compile('<div mydirective name="name">hi</div>')(scope);
          expect(element.text()).toBe('hello, world');
          */
 
