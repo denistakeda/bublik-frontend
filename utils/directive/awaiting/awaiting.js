049ad0378b5f5f28c2723fa8298d579595cbac93
den <denis.takeda@gmail.com>
2014-09-12 22:19:52 +0600

    feat(awaiting): animation for awaiting service

--- utils/directive/awaiting/awaiting.js
+++ utils/directive/awaiting/awaiting.js
@@ -1,22 +1,30 @@
-angular.module('glxUtils').directive('glxAwaiting', function (glxApplicationReady) {
+angular.module('glxUtils').directive('glxAwaiting', function (glxApplicationReady, $timeout) {
     return {
-        restrict: "A",
+        restrict: 'A',
         transclude: true,
         templateUrl: 'utils/directive/awaiting/awaiting.html',
         scope: {
-            glxAwaiting: "@glxAwaiting",
-            glxSpinnerType: "@glxSpinnerType"
+            glxAwaiting: '@glxAwaiting'
         },
-        link: function (scope) {
-                scope.isReady = function(){
-                    if (scope.glxAwaiting === 'application'){
-                        return glxApplicationReady.isResourceReady('localization') &&
-                            glxApplicationReady.isResourceReady('currentUser');
-                    } else {
-                        return glxApplicationReady.isResourceReady(scope.glxAwaiting);
-                    }
-                };
+        link: function (scope, element, attrs) {
+            var hideClass = attrs.glxHideClass || 'glx-full-transparent';
+            var showClass = attrs.glxShowClass || 'glx-full-untransparent';
+            scope.glxSpinnerType = attrs.glxSpinnerType;
 
+            scope.isReady = function () {
+                var ready = scope.glxAwaiting === 'application' &&
+                    glxApplicationReady.isResourceReady('localization') &&
+                    glxApplicationReady.isResourceReady('currentUser')||
+
+                    glxApplicationReady.isResourceReady(scope.glxAwaiting);
+                if (ready) {
+                    scope.contentClass = showClass;
+                } else {
+                    scope.contentClass = hideClass;
+                }
+
+                return ready;
+            };
         }
     };
 });
