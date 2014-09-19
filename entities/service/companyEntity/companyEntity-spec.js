ecda292068546cc023690b2add9c082e462f716d
den <denis.takeda@gmail.com>
2014-09-19 21:13:10 +0600

    feat(company): added title and avatar for company page

--- entities/service/companyEntity/companyEntity-spec.js
+++ entities/service/companyEntity/companyEntity-spec.js
@@ -1,17 +1,21 @@
-describe('glxCompanyEntity', function() {
+describe('glxCompanyEntity', function () {
 
     var companyId = 100500;
     var httpBackend;
 
-    beforeEach(module('glxEntities'));
-    beforeEach(inject(function($httpBackend){
+    beforeEach(module('glxEntities', function ($provide) {
+        $provide.factory('glxMessager', function () {
+            return {showErrorAlert: function () {}};
+        });
+    }));
+    beforeEach(inject(function ($httpBackend) {
         httpBackend = $httpBackend;
     }));
 
     it('createCompany function should return companyId', inject(function (glxCompanyEntity) {
         httpBackend.when('PUT', '/api/company/new').respond({data: {id: companyId}});
         var resultId;
-        glxCompanyEntity.createCompany({}, {title: "Company Name"}, function(data){
+        glxCompanyEntity.createCompany({}, {title: "Company Name"}, function (data) {
             resultId = data.id;
         });
         httpBackend.flush();
@@ -19,12 +23,21 @@ describe('glxCompanyEntity', function() {
 
     }));
 
-    it('should get company info', inject(function(glxCompanyEntity){
+    it('should get company info', inject(function (glxCompanyEntity) {
         var mockCompany = {id: 1, title: 'Some Company'};
         httpBackend.when('GET', '/api/company/1').respond({data: mockCompany});
-        glxCompanyEntity.getCompany({}, {companyId: 1});
+        glxCompanyEntity.getCompany({companyId: 1});
         httpBackend.flush();
         expect(glxCompanyEntity.storage.company).toEqual(mockCompany);
+    }));
+
+    it('should change company fields', inject(function (glxCompanyEntity) {
+        var mockFields = {title: 'New Comany Title', slogan: 'The best for a west'};
+        httpBackend.when('POST', '/api/company/1').respond({data: 'SUCCESS'});
+        glxCompanyEntity.setFields({companyId: 1}, mockFields);
+        httpBackend.flush();
+        expect(glxCompanyEntity.storage.company.title).toEqual(mockFields.title);
+        expect(glxCompanyEntity.storage.company.slogan).toEqual(mockFields.slogan);
     }));
 
 });
